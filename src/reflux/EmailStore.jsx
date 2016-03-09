a6b173677b57008334302bb41b588b1b4294749e
caraclarke <cara.clarke8@gmail.com>
2016-03-09 09:45:08 -0500

    createStore, .post added

--- src/reflux/EmailStore.jsx
+++ src/reflux/EmailStore.jsx
@@ -1,1 +1,24 @@
 var HTTP = require('../services/HttpService.js');
+var Reflux = require('reflux');
+var Actions = require('./actions.jsx');
+
+var EmailStore = Reflux.createStore({
+  listenables: [Actions],
+  submitEmail: function(subscriber) {
+    HTTP.post('/subscribers', subscriber)
+      .then(function(reponse) {
+        var msg = "";
+        
+        if (response.state === 200) {
+          msg = "Email submitted!";
+        } else {
+          msg = "Submission failed";
+        }
+        
+        this.trigger(msg);
+        // trigger what you call on reflux when you want to call something
+      }.bind(this));
+  }
+});
+
+module.exports = EmailStore;
